77e366be5638769a965c68e87dcd697c9750c6a9
clackypaas <clackypaas>
2024-09-19 11:45:48 +0800

    feat: update Main.java with new features and improvements

--- Main.java
+++ Main.java
@@ -1,6 +1,26 @@
+import org.elasticsearch.client.RestClient;
+import org.elasticsearch.client.RestHighLevelClient;
+
+public class ElasticSearchDemo {
+    public static void main(String[] args) {
+        // Create the client
+        try (RestHighLevelClient client = new RestHighLevelClient(
+                RestClient.builder(
+                        new HttpHost("localhost", 9200, "http")))) {
+
+            // Your operations go here
+            
+            System.out.println("Connected to ElasticSearch");
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
+
 public class Main {
     public static void main(final String[] args) {
-        System.out.print("hello world");
+        System.out.print("你好世界");
     }
 
 }
